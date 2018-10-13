f0a8f2538514fa4f99401859be7f2df84bddd092
yuriprym <samuel.young.103@gmail.com>
2018-10-13 01:15:53 +0000

    start of the orbtital basic design

---
+++ mysim.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+
+func fistfloor() *Number{
+   return nil
+}
+func isFirstFloor(x *Number) bool{
+   return x==nil
+}
+
+func isGoingUp(x *Number, y *Number)* number{
+   if isFirstFloor(x) {
+       return  goingUp(y)
+    }
+   return isgoingfloor(goingUp(y),goingDown(x))
+}
+func goingUp( x *Number) *Number {
+   if 
+
+
+func main()
