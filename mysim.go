82998765d27029648df4d24829d99103f10fbfd8
yuriprym <samuel.young.103@gmail.com>
2018-10-13 09:22:42 +0000

    I'm getting it to work

--- mysim.go
+++ mysim.go
@@ -1,22 +1,34 @@
 package main
 
+import "math"
 import "fmt"
 
-
-func fistfloor() *Number{
-   return nil
-}
-func isFirstFloor(x *Number) bool{
-   return x==nil
+type ele struct{
+  name string
+  bottomFloor,finalFloor float64
 }
 
-func goingUp(x , y *Number)* number{
-   if isFirstFloor(x) {
-       return  GoingUp(y)
-    }
-   return isgoingfloor(goingUp(y),goingDown(x))
+func isGoingUp(x,y float64)  float64{
+   if x >= y{
+     return x-y
+   }
+   return y-x
 }
-func isGoingUp( x *Number) *Number {
-   if 
-
-func main()
+func isGoingDown(x,y float64)  float64{
+   if x<=y {
+     return math.Abs(x-y)
+   }
+   return math.Abs(y-x)
+}
+func main() {
+ e:= ele{}
+ e.name= "Top-like zelda"
+ e.bottomFloor =  0.0
+ e.finalFloor = 1000.5
+ i := isGoingUp(e.bottomFloor,e.finalFloor)
+ n := isGoingDown(750.5,e.finalFloor)
+ fmt.Printf("%+v",e)
+ fmt.Println(i)
+ fmt.Println(n)
+ fmt.Println("hello W0rld")
+}
