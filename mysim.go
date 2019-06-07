f9d9687e34b47e7186a5fd4ebeab3ff5b1c325fa
yuriprym <samuel.young.103@gmail.com>
2019-06-06 22:08:13 -0400

    add in the lisences

--- mysim.go
+++ mysim.go
@@ -1,3 +1,9 @@
+/*Copyright 2018 the Space elevator simiulation Authors. All rights reseverd.
+use of this source code is governed by a GPL-style
+license that can be found in the LICENSE file.
+
+
+*/
 package main
 
 import(
